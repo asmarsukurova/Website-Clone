3d95bbe5f11d84d6ec028876ae3805fc3930a3d1
mahmudovarahide <mahmudovarahide@gmail.com>
2023-07-02 11:55:49 +0400

    javascript

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+// var
+//let
+// const
+
+// string
+// var name = "Asmar";
+
+// const name = "Asmar";
+
+// number
+let age = 22;
+
+// boolean
+// var school = false;
+
+
+
+var companyName = "GoWeb";
+
+let emekdas = "Rahide";
+let emekdas2 = "Esmer";
+let emekdas3 = "Aksin";
+
+var price = 600;
+var price2 = 700;
+var price3 = 1000;
+
+var totalPrice = price + price2 + price3 
+console.log(corey)
