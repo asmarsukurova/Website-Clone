9e740daf75a256fcd3c823e211e17ff7b90b31b4
mahmudovarahide <mahmudovarahide@gmail.com>
2023-07-05 16:20:20 +0400

    sehvlerrr

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,22 +8,22 @@
 // const name = "Asmar";
 
 // number
-let age = 22;
+// let age = 22;
 
 // boolean
 // var school = false;
 
 
 
-var companyName = "GoWeb";
+// var companyName = "GoWeb";
 
-let emekdas = "Rahide";
-let emekdas2 = "Esmer";
-let emekdas3 = "Aksin";
+// let emekdas = "Rahide";
+// let emekdas2 = "Esmer";
+// let emekdas3 = "Aksin";
 
-var price = 600;
-var price2 = 700;
-var price3 = 1000;
+// var price = 600;
+// var price2 = 700;
+// var price3 = 1000;
 
-var totalPrice = price + price2 + price3 
-console.log(corey)
+// var totalPrice = price + price2 + price3 
+// console.log(corey)
