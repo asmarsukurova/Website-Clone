402958a50f512a59ee74b608213eb9335a9ccebf
asmarsukurova <133567768+asmarsukurova@users.noreply.github.com>
2023-07-07 17:57:18 +0400

    new website

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,30 @@
-// let name = "Kenan";
-// var price = 20000;
-// let maas1 = 600;
-// let maas2 = 600;
-// let maas3 = 600;
-// let maas4 = 600;
-// let maas5 = 600;
-// let maas6 = 700;
-// let totalMaas = maas1 + maas2 + maas3 + maas4 + maas5 + maas6;
-// let totalFaiz = (totalMaas * 25) / 100;
-// console.log(totalFaiz)
-// var qalanGelir = price - totalFaiz;
+let name = "Kenan";
+var price = 20000;
+let maas1 = 600;
+let maas2 = 600;
+let maas3 = 600;
+let maas4 = 600;
+let maas5 = 600;
+let maas6 = 700;
+let totalMaas = maas1 + maas2 + maas3 + maas4 + maas5 + maas6;
+let totalFaiz = (totalMaas * 25) / 100;
+console.log(totalFaiz)
+var qalanGelir = price - totalFaiz;
+console.log(qalanGelir);
 
-// console.log(qalanGelir);
 
-let name = "Rahide";
-// console.log(name.length);
+let text = "Apple, Banana, Kiwi";
+let part = text.slice(7, 13);
+console.log(part)
 
-// let text = "Apple, Banana, Kiwi";
-// let part = text.slice(7, 13);
-// console.log(part)
 let cumle = "Menim 13 yasim var";
 console.log(cumle);
 var replace = cumle.replace("13", "14");
 console.log(replace);
+
+
+var malinQiymeti = 150
+var satilanMallar = 300
+let total = malinQiymeti * satilanMallar
+let = "satılan mallar"
+console.log('total' + ' ' + 'satılan mallar')
