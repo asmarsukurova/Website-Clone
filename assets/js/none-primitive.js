606dcc631923cac13dd9b7649d120fd25de97731
mahmudovarahide <mahmudovarahide@gmail.com>
2023-07-09 12:26:21 +0400

    javaScript objets and arrays

---
+++ assets/js/none-primitive.js
@@ -0,0 +1,54 @@
+// var name = "Rahide";
+// var name2 = "Aqsin";
+// var name3 = "asmar";
+// var name4= 'Pervae';
+
+const names = ["Rahide", "Aqsin", "Asmar"];
+
+console.log(names);
+
+names.push("Pervane");
+console.log(names);
+
+const Cars = [
+  {
+    name: "BMW",
+    year: 2018,
+    color: "red",
+  },
+  {
+    name: "AUDI",
+    year: 2012,
+    color: "black",
+  },
+];
+console.log(Cars);
+
+var reng = "red";
+console.log(reng);
+
+// var users = [];
+// console.log(users);
+const users = [
+  {
+    username: "asmarsukurovaaa",
+    age: 13,
+    followers: 71,
+    birthday: "11 / 02 / 2010",
+  },
+
+  {
+    name: "mahmudova.rahide",
+    age: 22,
+    follewers: 100,
+    birthday: "16 / 03 / 2001",
+  },
+  {
+    name: "roya_nails_masazir",
+    age: 37,
+    followers: "22k",
+    birthday: "23/10/1985",
+  },
+];
+
+console.log(users );
